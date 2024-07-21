dcd0ccbdb8aa504f5557ce52186918302d38b975
kim jiyeong <youngdev96@naver.com>
2024-07-21 22:20:09 +0900

    feat : 게임 시작 시, 시작문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-class App {
-  async play() {}
-}
+import { Console, Random } from "@woowacourse/mission-utils";
+import MESSAGE from "./view/constants.js";
 
+class App {
+  async play() {
+    await this.getCarsName();
+  }
+  //실행
+  async getCarsName() {
+    const carsName = await Console.readLineAsync(MESSAGE.CAR_NAME_PROMPT);
+    const carNameList = this.validateCarName(carsName);
+    this.runGame(carNameList);
+  }
+}
 export default App;
